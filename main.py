c2e9891983a5b7caeadf16fd076c169df3fe2d87
Puthearath Aim <puthearathaim@protonmail.com>
2025-04-20 11:17:16 +0700

    cleaned up calibrate_grid_size() function

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import keyboard
 import pyautogui
 import time
 
+import sudoku_writer
+import sudoku_reader
+import sudoku_solver
+
+
 def calibrate_grid_size():
     
     print("")
@@ -11,42 +16,59 @@ def calibrate_grid_size():
     print("3. Move mouse to the BOTTOM-RIGHT-MOST cell and press 's'.")
     print("")
 
-    while not keyboard.is_pressed('s'):
-        continue
+    def record_mouse_position(key_to_press: str = 's'):
+        while not keyboard.is_pressed(key_to_press):
+            continue
+        return pyautogui.position()
 
-    x1, y1 = pyautogui.position()
+    x1, y1 = record_mouse_position(key_to_press='s')
     print(f"Recorded top-left-most cell: ({x1}, {y1}) px")
 
     time.sleep(1)
-
-    while not keyboard.is_pressed('s'):
-        continue
     
-    x2, y2 = pyautogui.position()
+    x2, y2 = record_mouse_position(key_to_press='s')
     print(f"Recorded center cell of top-left box: ({x2}, {y2}) px")
 
     time.sleep(1)
 
-    while not keyboard.is_pressed('s'):
-        continue
-
-    x3, y3 = pyautogui.position()
+    x3, y3 = record_mouse_position(key_to_press='s')
     print(f"Recorded bottom-right-most cell: ({x3}, {y3}) px")
 
+    def calculate_cell_size(x1, y1, x2, y2):
+        # assert x2 - x1 > 7
+        cell_size_x = x2 - x1 # abs(x2 - x1)
+        cell_size_y = y2 - y1 # abs(y2 - y1)
+        cell_size_avg = (cell_size_x - cell_size_y) / 2
+        return int(cell_size_avg)
+    
+    cell_size = calculate_cell_size(x1, y1, x2, y2)
 
-    cell_size = int((abs(x2 - x1) + abs(y2 - y1)) / 2)
-    # abs(y3 - y1) = box_gap_y * 2 + cell_size * 8
-    # => box_gap_y = (abs(y3 - y1) - cell_size * 8) / 2
-    box_gap = int((abs(x3 - x1) + abs(y3 - y1) - cell_size * 16) / 4)
+    def calculate_box_gap(x1, y1, x3, y3, cell_size):
+        # assert x3 - x1 > 7 * 8 = 56
+        # start derivation
+        # abs(y3 - y1) = box_gap_y * 2 + cell_size * 8
+        # => box_gap_y = (abs(y3 - y1) - cell_size * 8) / 2
+        # end derivation
+        box_gap_x = x3 - x1
+        box_gap_y = y3 - y1
+        box_gap_avg = (box_gap_x + box_gap_y - (cell_size * 16)) / 4
+        return int(box_gap_avg)
+    
+    box_gap = calculate_box_gap(x1, y1, x3, y3, cell_size)
+
     print("")
     print(f"Estimated cell size: {cell_size} px")
     print(f"Estimated box gap: {box_gap} px")
-    
-    # assert x2-x1>7
-    # assert box gap is not negative
+
+    return x1, y1, cell_size, box_gap
+
 
 def main():
-    calibrate_grid_size()
+    x_topleft, y_topleft, cell_size, box_gap = calibrate_grid_size()
+
+    board = sudoku_reader.fill_board_from_screenshot(x_topleft, y_topleft, cell_size, box_gap)
+    solution = sudoku_solver.backtrack_solve(board)
+    sudoku_writer.move_through_board(x_topleft, y_topleft, cell_size, box_gap, solution[0])
 
 
 if __name__ == "__main__":
